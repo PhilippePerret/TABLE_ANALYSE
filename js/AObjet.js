8febbc050ddcf4cbecaf8f0786a6292f5de8d488
Philippe Perret <philippe.perret@yahoo.fr>
2021-09-17 11:23:47 +0200

    Fixed bug #14, #20

--- js/AObjet.js
+++ js/AObjet.js
@@ -113,6 +113,7 @@ class AObjet {
   set id(v){this._id = v}
 
   get height(){return this._height || (this._height = this.obj.offsetHeight)}
+  set height(v){this._height = v}
   get top(){return this._top || (this._top = this.obj.offsetTop)}
   set top(v){this._top = v; this.obj && (this.obj.style.top = px(v))}
 
